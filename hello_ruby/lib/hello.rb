6c74f4313f9c8a34e83dfc64c572ddc694f62789
Attila Domokos <adomokos@gmail.com>
2016-05-30 20:54:17 -0500

    Talk to the RDS instance with AR

--- hello_ruby/lib/hello.rb
+++ hello_ruby/lib/hello.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'faker'
+require 'active_record'
 
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql2",
+  :host     => "myinstance01.cgic5q3lz0bb.us-east-1.rds.amazonaws.com",
+  :username => "master",
+  :password => "Kew2401Sd",
+  :database => "awslambdaruby"
+)
+
+class User < ActiveRecord::Base
+end
+
+puts "Number of users: #{User.count}"
+puts "First user: #{User.first.firstname} #{User.first.lastname}"
 puts "Hello - '#{Faker::Name.name}' from Ruby!"
